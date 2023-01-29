44d1819f2f6c9cacc4cf1b067aefd04c1f69f488
keremcanskr <kerem.4555@gmail.com>
2023-01-29 21:00:51 +0300

    function for check box coded

--- todoapp/lib/screen/home_page.dart
+++ todoapp/lib/screen/home_page.dart
@@ -2,39 +2,114 @@ import 'package:flutter/material.dart';
 import 'package:todoapp/constant/colors.dart';
 import 'package:todoapp/widgets/todo_item.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+import '../models/todo.dart';
+
+class Home extends StatefulWidget {
+  Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final toDoList = ToDo.todoList();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: ToDoColors.appbarcolor,
         appBar: myappbar(),
-        body: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-          child: Column(
-            children: [
-              mysearchbar(),
-              Expanded(
-                child: ListView(
-                  children: [
-                    Container(
-                        margin: const EdgeInsets.only(top: 35, bottom: 20),
-                        child: const Text(
-                          "All To Dos",
-                          style: TextStyle(
-                              fontSize: 30, fontWeight: FontWeight.w500),
-                        )),
-                    const ToDoItem(),
-                    const ToDoItem(),
-                    const ToDoItem(),
-                    const ToDoItem(),
-                  ],
+        body: Stack(children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            child: Column(
+              children: [
+                mysearchbar(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                          margin: const EdgeInsets.only(top: 35, bottom: 20),
+                          child: const Text(
+                            "All To Dos",
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.w500),
+                          )),
+                      for (ToDo todoo in toDoList)
+                        ToDoItem(
+                          todo: todoo,
+                          onToDoChanged: _handleToDoChange,
+                          onDeleteItem: () {},
+                        ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ));
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                        bottom: 20,
+                        right: 20,
+                        left: 20,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 5,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 10.0,
+                            spreadRadius: 0.0,
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Add a new todo item',
+                            border: InputBorder.none),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(
+                      bottom: 20,
+                      right: 20,
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: ToDoColors.tdBlue,
+                        minimumSize: const Size(60, 60),
+                        elevation: 10,
+                      ),
+                      child: const Text(
+                        '+',
+                        style: TextStyle(
+                          fontSize: 40,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ))
+        ]));
+  }
+
+  void _handleToDoChange(ToDo todo) {
+    setState(() {
+      todo.isDone = !todo.isDone;
+    });
   }
 }
 
--- todoapp/lib/widgets/todo_item.dart
+++ todoapp/lib/widgets/todo_item.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/constant/colors.dart';
 
+import '../models/todo.dart';
+
 class ToDoItem extends StatelessWidget {
-  const ToDoItem({super.key});
+  const ToDoItem(
+      {super.key, required this.todo, this.onToDoChanged, this.onDeleteItem});
+  final ToDo todo;
+  final onToDoChanged;
+  final onDeleteItem;
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +16,18 @@ class ToDoItem extends StatelessWidget {
       margin: const EdgeInsets.only(bottom: 20),
       child: ListTile(
         onTap: () {
-          print('it did');
+          onToDoChanged(todo);
         },
         contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
         tileColor: Colors.white,
-        leading: const Icon(Icons.check_box, color: ToDoColors.tdBlue),
-        title: const Text("home work",
+        leading: Icon(
+            todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+            color: ToDoColors.tdBlue),
+        title: Text(todo.todoText!,
             style: TextStyle(
-                fontSize: 17, decoration: TextDecoration.lineThrough)),
+                fontSize: 17,
+                decoration: todo.isDone ? TextDecoration.lineThrough : null)),
         trailing: Container(
             padding: const EdgeInsets.all(0),
             margin: const EdgeInsets.symmetric(vertical: 12),
